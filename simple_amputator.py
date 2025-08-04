93768439b485730873be8bd3dd66d31199fcad58
pkorba <32198274+pkorba@users.noreply.github.com>
2025-08-04 15:55:57 +0200

    Fix a bug that prevents checking multiple URLs if bot cannot download the content of one of them, refactor code, add PyDocStrings

--- simple_amputator.py
+++ simple_amputator.py
@@ -13,40 +13,24 @@ class SimpleAmputatorBot(Plugin):
         if evt.sender == self.client.mxid:
             return
         await evt.mark_read()
-        deamped_urls = []
-        for url in matches:
-            text = await self.fetch_page_content(url[1])
-            if not text:
-                return
-            deamped_url = await asyncio.get_event_loop().run_in_executor(None, self.extract_canonical_url, url[1], text)
-            if deamped_url:
-                deamped_urls.append(deamped_url)
-        if not deamped_urls:
+        canonical_urls = await self.get_canonical_urls(matches)
+        if not canonical_urls:
             return
 
-        html = (
-            f"<blockquote>"
-            f"It looks like your message contains a Google AMP link. Here's a clean link:<br>• "
-            f"{'<br>• '.join(deamped_urls)}"
-            f"</blockquote>"
-        )
-        body = (
-            f"> It looks like your message contains a Google AMP link. Here's a clean link:  \n> • "
-            f"{'  \n> • '.join(deamped_urls)}"
-        )
-        content = TextMessageEventContent(
-            msgtype=MessageType.NOTICE,
-            format=Format.HTML,
-            body=body,
-            formatted_body=html)
+        content = await self.prepare_message(canonical_urls)
         await evt.respond(content)
 
     async def fetch_page_content(self, url: str) -> str:
+        """
+        Downloads URL content
+        :param url: website address
+        :return: website text
+        """
         headers = {
             "Sec-GPC": "1",
             "accept-encoding": "gzip, deflate, br, zstd",
-            "accept-language": "pl,en-US;q=0.7,en;q=0.3",
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:139.0) Gecko/20100101 Firefox/139.0",
+            "accept-language": "en,en-US;q=0.5",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:140.0) Gecko/20100101 Firefox/140.0",
             "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
         }
         try:
@@ -63,6 +47,12 @@ class SimpleAmputatorBot(Plugin):
             return ""
 
     def extract_canonical_url(self, url: str, text: str) -> str:
+        """
+        Extract canonical URL from a website
+        :param url: original URL
+        :param text: website text content
+        :return: canonical URL
+        """
         soup = BeautifulSoup(text, "html.parser")
         amp_link = None
         canonical_link = None
@@ -77,8 +67,13 @@ class SimpleAmputatorBot(Plugin):
                 return canonical_link
         return ""
 
-    @staticmethod
-    def urls_match(url_string: str, url_string2: str) -> bool:
+    def urls_match(self, url_string: str, url_string2: str) -> bool:
+        """
+        Compare two URLs
+        :param url_string: URL 1
+        :param url_string2: URL 2
+        :return: True if addresses match, False otherwise
+        """
         if url_string is None or url_string2 is None:
             return False
         if url_string.endswith("/"):
@@ -88,3 +83,41 @@ class SimpleAmputatorBot(Plugin):
         url1 = urlparse(url_string)
         url2 = urlparse(url_string2)
         return url1.netloc == url2.netloc and url1.path == url2.path and url1.query == url2.query and url1.params == url2.params
+
+    async def get_canonical_urls(self, urls: list[tuple[str, str]]) -> list[str]:
+        """
+        Extract canonical URLs from a list of URLs
+        :param urls: list of URLs
+        :return: list of canonical URLs
+        """
+        canonical_urls = []
+        for url in urls:
+            text = await self.fetch_page_content(url[1])
+            if not text:
+                continue
+            canonical_url = await asyncio.get_event_loop().run_in_executor(None, self.extract_canonical_url, url[1], text)
+            if canonical_url:
+                canonical_urls.append(canonical_url)
+        return canonical_urls
+
+    async def prepare_message(self, canonical_urls: list[str]) -> TextMessageEventContent:
+        """
+        Prepare message with list of canonical URLs
+        :param canonical_urls: list of canonical URLs
+        :return: text message
+        """
+        html = (
+            f"<blockquote>"
+            f"It looks like your message contains a Google AMP link. Here's a clean link:<br>• "
+            f"{'<br>• '.join(canonical_urls)}"
+            f"</blockquote>"
+        )
+        body = (
+            f"> It looks like your message contains a Google AMP link. Here's a clean link:  \n> • "
+            f"{'  \n> • '.join(canonical_urls)}"
+        )
+        return TextMessageEventContent(
+            msgtype=MessageType.NOTICE,
+            format=Format.HTML,
+            body=body,
+            formatted_body=html)
